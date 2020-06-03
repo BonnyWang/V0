e429081ca9f74c3cc6912667ae8088a9822a6aa6
BonnyWang <bonny.y.gardy@gmail.com>
2020-06-03 12:06:55 -0400

    CombatT

---
+++ Assets/Enemy_Attack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Attack : MonoBehaviour
+{
+    AnimCon mAnimCon;
+    void Start()
+    {
+        mAnimCon = new AnimCon(GetComponent<Animator>());
+    }
+
+    private void Update() {
+        
+    }
+    
+    private void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "Player"){
+            mAnimCon.backBounce(gameObject,other.gameObject);
+            Debug.Log("Enemy Debounced");
+        }
+    }
+
+    void detectPlayer(){
+        
+    }
+}
