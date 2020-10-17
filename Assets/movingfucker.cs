409bc59b381f5692cad8a316e7941f20a2964f61
AlexM <alex>
2020-10-17 19:55:59 +0800

    pausemenu

---
+++ Assets/movingfucker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingfucker : MonoBehaviour
+{
+    private bool a = true;
+
+
+    void Update()
+    {
+        if (gameObject.transform.position.x > 500)
+        {
+            a = false;
+        }
+        if (gameObject.transform.position.x < -500)
+        {
+            a = true;
+        }
+        if (a)
+        {
+            transform.Translate(100 * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.Translate(-100 * Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
---
+++ Assets/pausemenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausemenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+    [SerializeField] public GameObject PauseMenu;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+
+    void ResumeGame()
+    {
+        IsPaused = false;
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+    }
+
+    void PauseGame()
+    {
+        IsPaused = true;
+        Time.timeScale = 0f;
+        PauseMenu.SetActive(true);
+    }
+}
