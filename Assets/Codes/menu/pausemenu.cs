31bed68168ccc1f8da8fbd5cd9a0dd83631b20d3
AlexM <alex>
2020-10-28 01:27:11 +0800

    getbuttondown

--- Assets/Codes/menu/pausemenu.cs
+++ Assets/Codes/menu/pausemenu.cs
@@ -11,7 +11,7 @@ public class pausemenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetButtonDown("Cancel"))
         {
             if (IsPaused)
             {
